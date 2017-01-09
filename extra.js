4b9aa0d15f8558102eb0db5f02122f4ca07e0e7e
Andrea Baty <Andrea.f.baty@gmail.com>
2017-01-09 17:25:00 -0500

    final hope

--- extra.js
+++ extra.js
@@ -1,7 +1,7 @@
 
-function changeImage(imgid)
-{
-  if(imgid=='smilingcat')
+function changeImage()
   {
-  document.getElementById(imgid).src="https://i2.wp.com/100happybirthdaymemes.com/wp-content/uploads/2016/06/cat-meme-218141_w1000-1.jpg");
+  document.getElementById("smilingcat").src="https://i2.wp.com/100happybirthdaymemes.com/wp-content/uploads/2016/06/cat-meme-218141_w1000-1.jpg";
 }
+
+document.getElementById("theButton").addEventListener("click", changeImage)
