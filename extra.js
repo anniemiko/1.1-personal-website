348dd245fd46d25f7dd15f5b45c31dd03027fc7d
Andrea Baty <Andrea.f.baty@gmail.com>
2017-01-09 17:16:49 -0500

    commit again

---
+++ extra.js
@@ -0,0 +1,7 @@
+
+function changeImage(imgid)
+{
+  if(imgid=='smilingcat')
+  {
+  document.getElementById(imgid).src="https://i2.wp.com/100happybirthdaymemes.com/wp-content/uploads/2016/06/cat-meme-218141_w1000-1.jpg");
+}
